fa33428da5c72e11760472f005d4c24eaabd4bd5
filipb025 <filip.bogicevic025@hotmail.com>
2022-05-25 12:48:37 +0200

    Taking photo on iOS(PERMISSION)

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,8 +1,29 @@
-import {View, Button} from 'react-native'
-import {launchCameraAsync} from 'expo-image-picker'
+import {View, Button, Alert} from 'react-native'
+import {launchCameraAsync, useCameraPermissions, PermissionStatus} from 'expo-image-picker'
 
 function ImagePicker() {
+    const [cameraPermissionInformation, requestPermission] = useCameraPermissions()
+
+    async function verifyPermissions() {
+        if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+            const permissionResponse = await requestPermission()
+
+            return permissionResponse.granted
+        }
+
+        if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+            Alert.alert('Insufficient Permissions!')
+            return false
+        }
+        return true
+    }
+
     async function takeImageHandler() {
+        const hasPermission = await verifyPermissions()
+
+        if (!hasPermission) {
+            return
+        }
         const image = await launchCameraAsync({
             allowsEditing: true,
             aspect: [16, 9],
