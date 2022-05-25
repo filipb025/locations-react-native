5b1cfb91d70b372442da50f7da24623ac4872d8e
filipb025 <filip.bogicevic025@hotmail.com>
2022-05-25 09:18:16 +0200

    Editing the favorite places items

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,13 +1,35 @@
-import {FlatList} from "react-native";
+import {FlatList, StyleSheet, Text, View} from "react-native";
+import PlaceItem from "./PlaceItem";
 
 function PlacesList({places}) {
+
+    if (!places || places.length === 0) {
+        return (
+            <View style={styles.fallbackContainer}>
+                <Text style={styles.fallBackText}>
+                    No places added yet - start adding some!
+                </Text>
+            </View>
+        )
+    }
     return (
         <FlatList
             data={places}
-            keyExtractor={(item)=> item.id}
-            renderItem={}
+            keyExtractor={(item) => item.id}
+            renderItem={({item}) => <PlaceItem place={item}/>}
         />
     )
 }
+
+const styles = StyleSheet.create({
+    fallbackContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    fallBackText: {
+        fontSize: 16
+    }
+})
 
 export default PlacesList
