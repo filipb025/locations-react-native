3a394209bf93b1b1e99018ec83f4566762e811b6
filipb025 <filip.bogicevic025@hotmail.com>
2022-05-25 14:33:15 +0200

    custom button

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,7 +1,8 @@
-import {View, Button, Alert, Image, Text, StyleSheet} from 'react-native'
+import {View, Alert, Image, Text, StyleSheet} from 'react-native'
 import {launchCameraAsync, useCameraPermissions, PermissionStatus} from 'expo-image-picker'
 import {useState} from 'react'
 import {Colors} from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 function ImagePicker() {
     const [cameraPermissionInformation, requestPermission] = useCameraPermissions()
@@ -44,7 +45,12 @@ function ImagePicker() {
     return (
         <View>
             <View style={styles.imagePreview}>{imagePreview}</View>
-            <Button title='Take Image' onPress={takeImageHandler}/>
+            <OutlinedButton
+                icon="camera"
+                onPress={takeImageHandler}
+            >
+                Take Image
+            </OutlinedButton>
         </View>
     )
 }
